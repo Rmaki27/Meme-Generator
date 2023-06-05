147dbb483fa9c0a0d88dc3c76bb0b38ecd4c0c23
Rmaki27 <engineermaki27@gmail.com>
2023-06-04 17:11:22 -0700

    Create Header component and render

---
+++ Meme-Generator/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <div className="Header--container">
+            <div className="Header--title">
+                {/* <img src="" alt="meme icon" /> */}
+                <h1>Meme Generator</h1>    
+            </div>
+        </div>
+    )
+}
