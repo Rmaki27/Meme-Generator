ad793aa87d085151044091f3d9a60b575965328d
Rmaki27 <engineermaki27@gmail.com>
2023-06-04 17:30:57 -0700

    Create and render form component

---
+++ Meme-Generator/src/components/Form.jsx
@@ -0,0 +1,9 @@
+export default function Form() {
+    return (
+<form action="">
+    <input type="text" placeholder="Enter top text here" />
+    <input type="text" placeholder="Enter bottom text here" />
+    <button>Get a new meme image </button>
+</form>
+        )
+}
