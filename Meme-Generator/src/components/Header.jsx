c73775b722ed964c2706fbdf49c207056baa6f62
Rmaki27 <engineermaki27@gmail.com>
2023-06-04 17:25:52 -0700

    Style navbar

--- Meme-Generator/src/components/Header.jsx
+++ Meme-Generator/src/components/Header.jsx
@@ -2,10 +2,11 @@
 export default function Header() {
     return (
         <div className="Header--container">
-            <div className="Header--title">
-                {/* <img src="" alt="meme icon" /> */}
-                <h1>Meme Generator</h1>    
+            <div className="Header--title-container">
+                <img className="Header--icon" src="../public/images/troll-face.png" alt="meme icon" />
+                <h1 className="Header--title">Meme Generator</h1>    
             </div>
+            <h2 className="Header--course-number">React Course - Project 3</h2>
         </div>
     )
 }
