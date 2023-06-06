9dc6dcc8fbaf157d5bcffecac3fea37cd2e805cc
Rmaki27 <engineermaki27@gmail.com>
2023-06-05 19:00:24 -0700

    Refactor state to be defined as an object

--- Meme-Generator/src/components/Meme.jsx
+++ Meme-Generator/src/components/Meme.jsx
@@ -2,24 +2,31 @@ import memes from '../memesData.js'
 import React from 'react'
 
 export default function Meme() {
-    let [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "One Does Not Simply",
+        bottomText: "Beat Patrick Mahomes in November",
+        randomImage: "http://i.imgflip.com/1bij.jpg",
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memes.data.memes);
 
 
     function getMemeImage() {
-        const memesArr = memes.data.memes
+        const memesArr = allMemeImages
         const randMemeIndex = Math.floor(Math.random()*memesArr.length)
-        setMemeImage(memesArr[randMemeIndex].url)
-
+        const url = memesArr[randMemeIndex].url
+        setMeme(prevMeme => ({...prevMeme, randomImage: url}))
     }
-    return (
-<main>
-    <div className="Meme--container" action="">
-        <input type="text" placeholder="Enter top text here" />
-        <input type="text" placeholder="Enter bottom text here" />
-        <button className="Meme--button" onClick={getMemeImage}>Get a new meme image 🖼 </button>
-    </div>
-    <img src={memeImage} className='Meme--img' alt="meme image" />
-</main>
 
-        )
+    return (
+        <main>
+            <div className="Meme--container" action="">
+                <input type="text" placeholder="Enter top text here" />
+                <input type="text" placeholder="Enter bottom text here" />
+                <button className="Meme--button" onClick={getMemeImage}>Get a new meme image 🖼 </button>
+            </div>
+            <img src={meme.randomImage} className='Meme--img' alt="meme image" />
+        </main>
+
+    )
 }
